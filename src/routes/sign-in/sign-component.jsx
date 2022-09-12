9378e2865c3c54747b0683c5b7b10fa998123001
juniorrodriguez <jrod.201@hotmail.com>
2022-09-12 15:20:35 -0400

    redirect user login

--- src/routes/sign-in/sign-component.jsx
+++ src/routes/sign-in/sign-component.jsx
@@ -1,18 +1,37 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
+  auth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  //   useEffect(async () => {
+  //     const res = await getRedirectResult(auth);
+  //     if (res) {
+  //         const userDocRef = await createUserDocumentFromAuth(res.user);
+  //     }
+  //   }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
+  //   const LogGoogleRedirect = async () => {
+  //     const { user } = await signInWithGoogleRedirect();
+  //   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
